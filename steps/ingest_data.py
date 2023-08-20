6e5eddb52f24d40c37ece963a8f05695e1f38cb2
karthikabba <karthikabba96@gmail.com>
2023-08-19 20:50:36 -0500

    added detailed comments for the methods defined

--- steps/ingest_data.py
+++ steps/ingest_data.py
@@ -5,9 +5,22 @@ from zenml import step
 
 class IngestData:
     def __init__(self, data_path: str):
+        """
+        Initializes the IngestData class with the given data path.
+        Args:
+            data_path (str): The path to the data file that needs to be ingested.
+        """
         self.data_path = data_path
 
     def get_data(self):
+        """
+        Reads and returns the data from the specified data path.
+
+        Uses pandas to read the CSV file specified in data_path and logs the process.
+
+        Returns:
+            pd.DataFrame: The ingested data as a pandas DataFrame.
+        """
         logging.info(f"Ingesting data from {self.data_path}")
         return pd.read_csv(self.data_path)
 
