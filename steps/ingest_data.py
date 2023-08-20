eb8e9f0fed624179f1b5c8695fa96b3713e8569f
karthikabba <karthikabba96@gmail.com>
2023-08-19 20:12:30 -0500

    Added ingest_data step for ZenML pipeline to handle CSV ingestion
    - Introduced the ingest_data function, marked with ZenML's @step decorator, to streamline the process of reading CSV files within a ZenML pipeline.
    - Utilized the IngestData class for the underlying data loading logic, encapsulating the pandas read_csv functionality.
    - Implemented robust error handling with logging to ensure transparency and ease of debugging in case of ingestion errors.

--- steps/ingest_data.py
+++ steps/ingest_data.py
@@ -3,7 +3,7 @@ import pandas
 
 from zenml import step
 
-class Ingest_Data:
+class IngestData:
     def __init__(self, data_path: str):
         self.data_path = data_path
 
@@ -11,3 +11,22 @@ class Ingest_Data:
         logging.info(f"Ingesting data from {self.data_path}")
         return pd.read_csv(self.data_path)
 
+@step
+def ingest_data(data_path: str) -> pd.DataFrame:
+    """
+    Ingesting the data from the data_path.
+
+    Args:
+        data_path: path to the data.
+    Return:
+        pd.datframe: the ingested data
+    """
+
+    try:
+        ingest_data = IngestData(data_path)
+        df = ingest_data.get_data()
+        return df
+
+    except Exception as e:
+        logging.error(f"Error while ingesting data : {e}")
+        raise e
