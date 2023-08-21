6573cedc012d7d84fa7c89e66f11418cf89d2b63
karthikabba <karthikabba96@gmail.com>
2023-08-21 17:31:41 -0500

    added hyperparameter tuning components to model_dev.py

--- src/evaluation.py
+++ src/evaluation.py
@@ -1,71 +1,89 @@
 import logging
-import numpy as np
-import pandas as pd
 from abc import ABC, abstractmethod
-from sklearn.linear_model import LinearRegression
+
+import numpy as np
 from sklearn.metrics import mean_squared_error, r2_score
+
 
 class Evaluation(ABC):
     """
-    Abstract class defining strategy for evaluating models
+    Abstract Class defining the strategy for evaluating model performance
     """
     @abstractmethod
-    def calculate_scores(self, y_true: np.ndarray, y_pred: np.ndarray):
-        """
-        Calculate the scores for the model
-        Args:
-            y_true: True label
-            y_pred: Predicted labels
-        Returns:
-            None
-        """
+    def calculate_score(self, y_true: np.ndarray, y_pred: np.ndarray) -> float:
         pass
+
 
 class MSE(Evaluation):
     """
-    Abstract class defining strategy for evaluating models
+    Evaluation strategy that uses Mean Squared Error (MSE)
     """
-
-    def calculate_scores(self, y_true: np.ndarray, y_pred: np.ndarray):
+    def calculate_score(self, y_true: np.ndarray, y_pred: np.ndarray) -> float:
+        """
+        Args:
+            y_true: np.ndarray
+            y_pred: np.ndarray
+        Returns:
+            mse: float
+        """
         try:
-            logging.info("Calculating MSE")
+            logging.info("Entered the calculate_score method of the MSE class")
             mse = mean_squared_error(y_true, y_pred)
-            logging.info("MSE: {}".format(mse))
+            logging.info("The mean squared error value is: " + str(mse))
             return mse
         except Exception as e:
-            logging.error ("Error in calculating MSE: {}".format(e))
+            logging.error(
+                "Exception occurred in calculate_score method of the MSE class. Exception message:  "
+                + str(e)
+            )
+            raise e
 
 
-class R2(Evaluation):
+class R2Score(Evaluation):
     """
-    Abstract class defining strategy for evaluating models
+    Evaluation strategy that uses R2 Score
     """
-
-    def calculate_scores(self, y_true: np.ndarray, y_pred: np.ndarray):
+    def calculate_score(self, y_true: np.ndarray, y_pred: np.ndarray) -> float:
+        """
+        Args:
+            y_true: np.ndarray
+            y_pred: np.ndarray
+        Returns:
+            r2_score: float
+        """
         try:
-            logging.info("Calculating R2")
+            logging.info("Entered the calculate_score method of the R2Score class")
             r2 = r2_score(y_true, y_pred)
-            logging.info("R2 score: {}".format(r2))
+            logging.info("The r2 score value is: " + str(r2))
             return r2
         except Exception as e:
-            logging.error ("Error in calculating R2 score: {}".format(e))
+            logging.error(
+                "Exception occurred in calculate_score method of the R2Score class. Exception message:  "
+                + str(e)
+            )
+            raise e
+
 
 class RMSE(Evaluation):
     """
-    Abstract class defining strategy for evaluating models
+    Evaluation strategy that uses Root Mean Squared Error (RMSE)
     """
-
-    def calculate_scores(self, y_true: np.ndarray, y_pred: np.ndarray):
+    def calculate_score(self, y_true: np.ndarray, y_pred: np.ndarray) -> float:
+        """
+        Args:
+            y_true: np.ndarray
+            y_pred: np.ndarray
+        Returns:
+            rmse: float
+        """
         try:
-            logging.info("Calculating RMSE")
+            logging.info("Entered the calculate_score method of the RMSE class")
             rmse = np.sqrt(mean_squared_error(y_true, y_pred))
-            logging.info("root_mean_squared_error: {}".format(rmse))
+            logging.info("The root mean squared error value is: " + str(rmse))
             return rmse
         except Exception as e:
-            #log error
-            logging.error ("Error in calculating root_mean_squared_error score: {}".format(e))
-
-
-
-
-            
+            logging.error(
+                "Exception occurred in calculate_score method of the RMSE class. Exception message:  "
+                + str(e)
+            )
+            raise e
